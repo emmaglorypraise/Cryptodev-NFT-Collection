f81416cbffff4c7259b0cb8dd4b1b2ca906aac68
emmaglorypraise <emmaglorypraise@gmail.com>
2022-04-18 13:44:56 +0100

    fixed art id error

--- nft-app/utils/arts.js
+++ nft-app/utils/arts.js
@@ -1,26 +1,26 @@
 const arts1 = [
     {
-      id: 1,
+      id: 0,
       image:
         "https://gateway.pinata.cloud/ipfs/QmcfsZCGdcXyHJP8MupMQwYuSET8TFJCHUhAaoJwe4EJuP/0.svg",
     },
     {
-      id: 2,
+      id: 1,
       image:
         "https://gateway.pinata.cloud/ipfs/QmcfsZCGdcXyHJP8MupMQwYuSET8TFJCHUhAaoJwe4EJuP/1.svg",
     },
     {
-      id: 3,
+      id: 2,
       image:
         "https://gateway.pinata.cloud/ipfs/QmcfsZCGdcXyHJP8MupMQwYuSET8TFJCHUhAaoJwe4EJuP/2.svg",
     },
     {
-      id: 4,
+      id: 3,
       image:
         "https://gateway.pinata.cloud/ipfs/QmcfsZCGdcXyHJP8MupMQwYuSET8TFJCHUhAaoJwe4EJuP/3.svg",
     },
     {
-      id: 5,
+      id: 4,
       image:
         "https://gateway.pinata.cloud/ipfs/QmcfsZCGdcXyHJP8MupMQwYuSET8TFJCHUhAaoJwe4EJuP/4.svg",
     },
@@ -28,27 +28,27 @@ const arts1 = [
   
   const arts2 = [
     {
-      id: 6,
+      id: 5,
       image:
         "https://gateway.pinata.cloud/ipfs/QmcfsZCGdcXyHJP8MupMQwYuSET8TFJCHUhAaoJwe4EJuP/5.svg",
     },
     {
-      id: 7,
+      id: 6,
       image:
         "https://gateway.pinata.cloud/ipfs/QmcfsZCGdcXyHJP8MupMQwYuSET8TFJCHUhAaoJwe4EJuP/6.svg",
     },
     {
-      id: 8,
+      id: 7,
       image:
         "https://gateway.pinata.cloud/ipfs/QmcfsZCGdcXyHJP8MupMQwYuSET8TFJCHUhAaoJwe4EJuP/7.svg",
     },
     {
-      id: 9,
+      id: 8,
       image:
         "https://gateway.pinata.cloud/ipfs/QmcfsZCGdcXyHJP8MupMQwYuSET8TFJCHUhAaoJwe4EJuP/8.svg",
     },
     {
-      id: 10,
+      id: 9,
       image:
         "https://gateway.pinata.cloud/ipfs/QmcfsZCGdcXyHJP8MupMQwYuSET8TFJCHUhAaoJwe4EJuP/9.svg",
     },
@@ -56,27 +56,27 @@ const arts1 = [
   
   const arts3 = [
     {
-      id: 11,
+      id: 10,
       image:
         "https://gateway.pinata.cloud/ipfs/QmcfsZCGdcXyHJP8MupMQwYuSET8TFJCHUhAaoJwe4EJuP/10.svg",
     },
     {
-      id: 12,
+      id: 11,
       image:
         "https://gateway.pinata.cloud/ipfs/QmcfsZCGdcXyHJP8MupMQwYuSET8TFJCHUhAaoJwe4EJuP/11.svg",
     },
     {
-      id: 13,
+      id: 12,
       image:
         "https://gateway.pinata.cloud/ipfs/QmcfsZCGdcXyHJP8MupMQwYuSET8TFJCHUhAaoJwe4EJuP/12.svg",
     },
     {
-      id: 14,
+      id: 13,
       image:
         "https://gateway.pinata.cloud/ipfs/QmcfsZCGdcXyHJP8MupMQwYuSET8TFJCHUhAaoJwe4EJuP/13.svg",
     },
     {
-      id: 15,
+      id: 14,
       image:
         "https://gateway.pinata.cloud/ipfs/QmcfsZCGdcXyHJP8MupMQwYuSET8TFJCHUhAaoJwe4EJuP/14.svg",
     },
@@ -84,27 +84,27 @@ const arts1 = [
   
   const arts4 = [
     {
-      id: 16,
+      id: 15,
       image:
         "https://gateway.pinata.cloud/ipfs/QmcfsZCGdcXyHJP8MupMQwYuSET8TFJCHUhAaoJwe4EJuP/15.svg",
     },
     {
-      id: 17,
+      id: 16,
       image:
         "https://gateway.pinata.cloud/ipfs/QmcfsZCGdcXyHJP8MupMQwYuSET8TFJCHUhAaoJwe4EJuP/16.svg",
     },
     {
-      id: 18,
+      id: 17,
       image:
         "https://gateway.pinata.cloud/ipfs/QmcfsZCGdcXyHJP8MupMQwYuSET8TFJCHUhAaoJwe4EJuP/17.svg",
     },
     {
-      id: 19,
+      id: 18,
       image:
         "https://gateway.pinata.cloud/ipfs/QmcfsZCGdcXyHJP8MupMQwYuSET8TFJCHUhAaoJwe4EJuP/18.svg",
     },
     {
-      id: 20,
+      id: 19,
       image:
         "https://gateway.pinata.cloud/ipfs/QmcfsZCGdcXyHJP8MupMQwYuSET8TFJCHUhAaoJwe4EJuP/19.svg",
     },
